341c6b492e4d717fe2f6dd4c61dbce773def2b56
Jon Rumsey <jon.rumsey@gmail.com>
2023-08-17 11:30:46 -0700

    implemented basic routes w/ logging

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 const server = express();
 import Watcher from 'watcher';
 import 'dotenv/config';
+import logger from 'utils/logger.js';
 
 const filePath = process.env.FILE_PATH;
 const watcherOptions = {
@@ -20,7 +21,55 @@ const watcherOptions = {
 
 var watcher = new Watcher();
 const PORT = process.env.PORT || 6002;
+server.use(logger);
 
 server.get('/', (req, res, next) => {
   res.send('Server is running.');
+});
+
+server.get('/start', (req, res, next) => {
+  watcher = new Watcher(filePath, watcherOptions);
+  console.log('Called file watcher START.');
+
+  watcher.on('error', (err) => {
+    console.error('Error:', err);
+  });
+  watcher.on('ready', () => {
+    console.log('File watcher ready.');
+  });
+  watcher.on('close', () => {
+    console.log('File watcher closed.');
+  });
+  watcher.on('add', (filePath) => {
+    console.log('File added:', filePath);
+    // todo: call function to process file
+  });
+  watcher.on('unlink', (filePath) => {
+    console.log('File removed:', filePath);
+    // todo: determine what to do when a file is removed (or renamed)
+  });
+
+  res.send('File watcher started.');
+});
+
+server.get('/stop', (req, res, next) => {
+  console.log('Called file watcher STOP.');
+  // call to stop file watcher
+  watcher.close();
+  res.send('File watcher stopped.');
+});
+
+server.use((err, req, res, next) => {
+  console.error('Error:', err);
+  if (res.headersSent) {
+    console.error(
+      'Headers already send, cannot respond to client with current error.'
+    );
+  } else {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log('Server listening on port', PORT);
 });
