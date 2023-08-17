087b28702da4c359e881163f50a8854d74ea01b5
Jon Rumsey <jon.rumsey@gmail.com>
2023-08-17 15:59:24 -0700

    removed console log from timestamper

--- src/utils/timestamper.js
+++ src/utils/timestamper.js
@@ -1,7 +1,4 @@
-function timeStamper() {
+export default function timeStamper() {
   const dateNow = new Date(Date.now());
-  console.log('timeStamper returning', dateNow);
   return dateNow;
 }
-
-module.exports = timeStamper;
